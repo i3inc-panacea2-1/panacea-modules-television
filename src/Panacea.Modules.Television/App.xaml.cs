f373717f2a9c8bab98138802843ac7c9fd8f4c65
Portno <j.remoundos@dotbydot.gr>
2019-06-24 13:17:47 +0300

    nuget + font sizes

---
+++ src/Panacea.Modules.Television/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Panacea.Modules.Television
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        public App()
+        {
+
+        }
+    }
+}
+
